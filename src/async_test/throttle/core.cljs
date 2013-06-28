e63e822cc68f16d5524d8497365ed1ae570d5a4f
David Nolen <david.nolen@gmail.com>
2013-06-28 19:37:48 -0400

    use sliding buffer in throttle example

--- src/async_test/throttle/core.cljs
+++ src/async_test/throttle/core.cljs
@@ -1,10 +1,10 @@
 (ns async-test.throttle.core
-  (:require [cljs.core.async :refer [chan close!]]
-            [clojure.string :as string])
+   (:require [cljs.core.async :refer [chan close! sliding-buffer]]
+             [clojure.string :as string])
   (:require-macros
     [cljs.core.async.macros :as m :refer [go alts!]]))
 
-(def c (chan))
+(def c (chan (sliding-buffer 1)))
 (def loc-div (.getElementById js/document "location"))
 
 (.addEventListener js/window "mousemove"
