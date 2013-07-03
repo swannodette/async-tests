def658f6279306aeeadec9294cd310e5b1bf698a
David Nolen <david.nolen@gmail.com>
2013-07-02 21:06:26 -0400

    ok we have something that works

--- src/async_test/autocomplete/core.cljs
+++ src/async_test/autocomplete/core.cljs
@@ -4,68 +4,47 @@
             [clojure.string :as string]
             [async-test.autocomplete.utils
              :refer [key-chan by-id throttle split-chan set-class
-                     clear-class]])
+                     clear-class timeout]])
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]
                    [clojure.core.match.js :refer [match]]
                    [async-test.autocomplete.macros :refer [go-loop]]))
-
-(defn text-chan
-  ([cc input-el] (text-chan (chan) cc input-el))
-  ([c cc input-el]
-    (go-loop
-      (<! cc)
-      (>! c (.-value input-el)))
-    c))
 
 (defn fetch [value]
   (let [c (chan)]
     (go
       (println "get data" value)
+      (<! (timeout (rand-int 300)))
       (>! c :data)
       (close! c))
     c))
-
-(defn close-all! [cs]
-  (doseq [c cs] (close! c)))
 
 (defn no-input? [e input-el]
   (let [code (.-keyCode e)]
     (and (= code 8) (string/blank? (.-value input-el)))))
 
-(defn cleanup [ac c ac-el]
-  (set-class ac-el "hidden")
-  (close! ac))
-
 (defn autocompleter* [c input-el ac-el]
-  (let [ac (chan)
-        [c] (split-chan c 1)
-        [c' c''] (split-chan c 2)
-        tc (text-chan (throttle c'' 500) input-el)
-        killc (chan)]
+  (let [ac (chan)]
     (clear-class ac-el)
-    (go (loop []
-          (let [e (<! c')]
-            (if (no-input? e input-el)
-              (do
-                (close! killc)
-                (recur))
-              (recur)))))
-    (go (loop []
-          (let [[v c] (alts! [tc killc])]
-            (if (= c killc)
-              (cleanup ac c ac-el)
-              (if-not (string/blank? v)
-                (do
-                  (alts! [(fetch v) killc])
-                  (recur))
-                (recur))))))
+    (go
+      (loop [start (js/Date.)]
+        (let [e (<! c)]
+          (cond
+            (no-input? e input-el)
+            (do (set-class ac-el "hidden")
+              (close! ac))
+            
+            (>= (- (js/Date.) start) 500)
+            (do (<! (fetch (.-value input-el)))
+              (recur (js/Date.)))
+
+            :else
+            (recur start)))))
     ac))
 
 (defn autocompleter [input-el ac-el]
   (let [kc (key-chan (chan (sliding-buffer 1)) input-el "keyup")
         [kc'] (split-chan kc 1)]
     (go-loop
-      (println "OPEN")
       (<! kc)
       (when (pos? (alength (.-value input-el)))
         (<! (autocompleter* kc' input-el ac-el))))))
