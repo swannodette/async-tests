3e7663615cf630c36c71f9bd3883df369a37d430
David Nolen <david.nolen@gmail.com>
2013-07-06 10:56:32 -0400

    still unhappy with the precision of throttle and after-last, they may be ready to go when the autocompleter process continues instead of being reset. we can pass channels around for this.

--- src/async_test/autocomplete/core.cljs
+++ src/async_test/autocomplete/core.cljs
@@ -75,3 +75,8 @@
   (by-id "input")
   (by-id "completions"))
 
+;; TODO: throttle & after-last should be stoppable, we could pass control
+;;       channel, which must be read from to continue, they can reset
+;;       themselves via this mechanism
+;; TODO: handle selection
+
