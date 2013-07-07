b6c5957378c0f90317a7bb9aa7692b9e964d8663
David Nolen <david.nolen@gmail.com>
2013-07-07 11:50:38 -0400

    big cleanup of autocomplete code now that throttle actually works

--- src/async_test/autocomplete/core.cljs
+++ src/async_test/autocomplete/core.cljs
@@ -6,11 +6,10 @@
             [async-test.utils.helpers
              :refer [event-chan by-id copy-chan set-class throttle
                      clear-class jsonp-chan set-html now multiplex
-                     after-last map-chan filter-chan fan-in debounce]])
+                     after-last map-chan filter-chan fan-in debounce
+                     distinct-chan]])
   (:require-macros [cljs.core.async.macros :as m :refer [go]]
                    [async-test.utils.macros :refer [go-loop]]))
-
-(def IGNORE #{98 16 37 38 39 40 18 20})
 
 (def base-url
   "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=")
@@ -23,62 +22,38 @@
       (apply str)
       (set-html rs))))
 
-(defn no-input [c el]
-  (->> c
-    (map-chan #(do % (.-value el)))
-    (filter-chan string/blank?)))
-
 (defn autocompleter*
-  [{start :start c :chan blur :blur} input-el ac-el]
+  [{c :chan blur :blur} input-el ac-el]
   (let [ac (chan)
-        [c' c'' c'''] (multiplex c 3)
-        no-input      (no-input c' input-el)
-        delay         (after-last c'' 300)
-        interval      (throttle c''' 500)
-        fetch         (debounce (fan-in [delay interval]) 500)]
+        [c' c''] (multiplex c 2)
+        no-input (filter-chan string/blank? c')
+        fetch    (throttle (filter-chan #(> (count %) 2) c'') 500)]
     (go
-      (<! start)
-      (loop []
+      (loop [cancel false]
         (let [[v sc] (alts! [blur no-input fetch])]
           (condp contains? sc
             #{no-input blur}
             (do (set-class ac-el "hidden")
-              (>! ac (.-value input-el))
-              (<! start)
-              (recur))
+              (recur true))
 
             #{fetch}
-            (let [r (<! (jsonp-chan (str base-url (.-value input-el))))]
-              (show-results r)
-              (recur))))))
+            (if-not cancel
+              (let [r (<! (jsonp-chan (str base-url v)))]
+                (show-results r)
+                (recur false))
+              (recur false))))))
     ac))
 
 (defn autocompleter [input-el ac-el]
-  (let [c (->> (:chan (event-chan input-el "keyup"))
-            (map-chan #(get % "keyCode"))
-            (filter-chan (complement IGNORE))) 
-        [kc kc'] (multiplex c [(chan (dropping-buffer 1)) (chan)])
-        ctrl {:start (chan)
-              :chan kc'
-              :blur (:chan (event-chan input-el "blur"))}
-        ac   (autocompleter* ctrl input-el ac-el)]
-    (go
-      (loop [query nil]
-        (<! kc)
-        (recur
-          (when (and (not= query (.-value input-el))
-                     (pos? (alength (.-value input-el))))
-            (>! (:start ctrl) (now))
-            (<! ac)))))))
+  (let [ctrl {:chan (distinct-chan
+                      (map-chan #(do % (.-value input-el))
+                        (:chan (event-chan input-el "keyup"))))
+              :blur (:chan (event-chan input-el "blur"))}]
+    (autocompleter* ctrl input-el ac-el)))
 
 (autocompleter
   (by-id "input")
   (by-id "completions"))
 
-;; TODO: throttle & after-last should be stoppable, we could pass control
-;;       channel, which must be read from to continue, they can reset
-;;       themselves via this mechanism
-;; TODO: closure tools show that spurious keys like Command
-;;       still trigger network activity
-;; TODO: handle selection
+;; TODO: handle arrow movemennt & selection
 
