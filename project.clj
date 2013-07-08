e98eb851d184f72e2c2ffea938a55ade9b223335
David Nolen <david.nolen@gmail.com>
2013-07-08 14:08:07 -0500

    changes in case I demo some stuff later

--- project.clj
+++ project.clj
@@ -16,35 +16,40 @@
   :cljsbuild
   {:builds
    [{:id "mouse"
-     :source-paths ["src/async_test/mouse"]
+     :source-paths ["src/async_test/mouse"
+                    "src/async_test/utils"]
      :compiler {:optimizations :whitespace
                 :pretty-print false
                 :static-fns true
-                :output-to "main.js"}}
+                :output-to "mouse.js"}}
     {:id "timeout"
-     :source-paths ["src/async_test/timeout"]
+     :source-paths ["src/async_test/timeout"
+                    "src/async_test/utils"]
      :compiler {:optimizations :whitespace
                 :pretty-print false
                 :static-fns true
-                :output-to "main.js"}}
+                :output-to "timeout.js"}}
     {:id "throttle"
-     :source-paths ["src/async_test/throttle"]
+     :source-paths ["src/async_test/throttle"
+                    "src/async_test/utils"]
      :compiler {:optimizations :whitespace
                 :pretty-print false
                 :static-fns true
-                :output-to "main.js"}}
+                :output-to "throttle.js"}}
     {:id "debounce"
-     :source-paths ["src/async_test/debounce"]
+     :source-paths ["src/async_test/debounce"
+                    "src/async_test/utils"]
      :compiler {:optimizations :whitespace
                 :pretty-print false
                 :static-fns true
-                :output-to "main.js"}}
+                :output-to "debounce.js"}}
     {:id "robpike"
-     :source-paths ["src/async_test/robpike"]
+     :source-paths ["src/async_test/robpike"
+                    "src/async_test/utils"]
      :compiler {:optimizations :whitespace
                 :pretty-print false
                 :static-fns true
-                :output-to "main.js"}}
+                :output-to "robpike.js"}}
     {:id "bench"
      :source-paths ["src/async_test/bench"]
      :compiler {:optimizations :whitespace
@@ -64,4 +69,4 @@
      :compiler {:optimizations :whitespace
                 :pretty-print false
                 :static-fns true
-                :output-to "main.js"}}]})
+                :output-to "binding.js"}}]})
--- src/async_test/mouse/core.cljs
+++ src/async_test/mouse/core.cljs
@@ -2,15 +2,15 @@
   (:require [cljs.core.async :refer [chan sliding-buffer put!]]
             [clojure.string :as string]
             [async-test.utils.helpers
-             :refer [event-chan set-html by-id]])
+            :refer [event-chan set-html by-id]])
   (:require-macros [cljs.core.async.macros :as m :refer [go alts!]]
                    [clojure.core.match.js :refer [match]]))
  
 (def loc-div (by-id "location"))
 (def key-div (by-id "key"))
 
-(def mc (event-chan js/window "mousemove"))
-(def kc (event-chan js/window "keyup"))
+(def mc (:chan (event-chan js/window "mousemove")))
+(def kc (:chan (event-chan js/window "keyup")))
  
 (defn handler [[e c]]
   (match [e]
