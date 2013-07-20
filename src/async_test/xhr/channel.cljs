6a07efce5a6d2d26b1407f57a242dc25e5144eed
Darrick Wiebe <dw@xnlogic.com>
2013-07-20 12:18:04 -0400

    Remove take and drop

--- src/async_test/xhr/channel.cljs
+++ src/async_test/xhr/channel.cljs
@@ -1,7 +1,7 @@
 (ns async-test.xhr.channel
-  (:refer-clojure :exclude [map mapcat take drop])
+  (:refer-clojure :exclude [map mapcat])
   (:require-macros
-    [cljs.core.async.macros :as m :refer [go]])
+    [cljs.core.async.macros :refer [go]])
   (:require [cljs.core.async :refer [<! >! chan close!]]))
 
 (defn map [f c]
@@ -28,27 +28,3 @@
                   (when-not (nil? v) (>! r v)))
                 (recur))))))
     r))
-
-(defn take [limit c]
-  (let [r (chan)]
-    (go
-      (loop [n 0]
-        (if (< n limit)
-          (let [v (<! c)]
-            (if (nil? v)
-              (close! r)
-              (do (>! r v)
-                  (recur (inc n)))))
-          (close! r))))
-    r))
-
-(defn drop [offset c]
-  (go
-    (loop [n 0]
-      (when (< n offset)
-        (let [v (<! c)]
-          (if (nil? v)
-            (close! c)
-            (recur (inc n)))))))
-  c)
-
