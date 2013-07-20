18362a605f26120d6c966d04b3f706a39ed2e61c
David Nolen <david.nolen@gmail.com>
2013-07-20 17:35:04 -0400

    update daisy settings for Node.js. use CLJS 1845, delete message perf experiments

--- project.clj
+++ project.clj
@@ -5,9 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1843"] 
+                 [org.clojure/clojurescript "0.0-1845-RC1"]
                  [org.clojure/core.match "0.2.0-rc2"]
                  [core.async "0.1.0-SNAPSHOT"]]
+  
+  :repositories {"sonatype-staging"
+                 "https://oss.sonatype.org/content/groups/staging/"}
 
   :plugins [[lein-cljsbuild "0.3.2"]]
 
@@ -23,9 +26,8 @@
     {:id "daisy"
      :source-paths ["src/async_test/daisy"
                     "src/async_test/utils"]
-     :compiler {:optimizations :whitespace
+     :compiler {:optimizations :simple
                 :pretty-print false
-                :static-fns true
                 :output-to "daisy.js"}}
     {:id "timeout"
      :source-paths ["src/async_test/timeout"
